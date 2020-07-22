146992624951735b92ae6c8d267d303903ae179d
AlexKov <kovalyshyn.alex@gmail.com>
2020-07-22 12:32:54 -0700

    Initial Commit

---
+++ FaceMoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceMoji
+//
+//  Created by Oleksandr Kovalyshyn on 22.07.2020.
+//  Copyright © 2020 Oleksandr Kovalyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
